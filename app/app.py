d3709e441c179eb6de4c6d682eeb0d89beb21c6f
MihailDDimovski <148362367+MihailDDimovski@users.noreply.github.com>
2024-02-18 17:56:27 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -180,4 +180,4 @@ if __name__ == '__main__':
     create_schema()
 
     # Start a webserver
-    app.run(port=int(os.environ.get('PORT', '5000')))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', '5000')))
